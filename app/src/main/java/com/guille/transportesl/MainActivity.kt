d85eabc2310c391151112de4f08333ca51ff60d4
Guille <desarrolladoroxidado@gmail.com>
2026-08-03 18:33:42 -0300

    Se crea pantalla inicial

--- app/src/main/java/com/guille/transportesl/MainActivity.kt
+++ app/src/main/java/com/guille/transportesl/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.guille.transportesl
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.guille.transportesl.ui.theme.TransporteSLTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,10 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TransporteSLTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PantallaInicial( modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -37,6 +46,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun PantallaInicial( modifier : Modifier = Modifier){
+   Column(
+       modifier = modifier
+           .fillMaxSize()
+           .padding(24.dp),
+       verticalArrangement = Arrangement.Center,
+       horizontalAlignment = Alignment.CenterHorizontally
+   ){
+       Text( text = "TransporteSL",
+             style = MaterialTheme.typography.headlineMedium)
+       Spacer( modifier = Modifier.height(12.dp))
+       Text( text = "Información del transporte público de San Luis")
+       Spacer( modifier = Modifier.height(24.dp))
+       Button(onClick = {})
+       {
+           Text( text = "Continuar",
+                 style = MaterialTheme.typography.titleLarge)
+       }
+   }
+
 }
 
 @Preview(showBackground = true)
