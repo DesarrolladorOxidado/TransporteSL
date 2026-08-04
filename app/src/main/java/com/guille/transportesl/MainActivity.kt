189b0cc1f0eed0fcd403ce538b51533478f5fd89
Guille <desarrolladoroxidado@gmail.com>
2026-08-04 18:12:53 -0300

    Se implementa estado en pantalla inicial

--- app/src/main/java/com/guille/transportesl/MainActivity.kt
+++ app/src/main/java/com/guille/transportesl/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.guille.transportesl
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,19 +10,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.guille.transportesl.ui.theme.TransporteSLTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,12 +57,17 @@ fun PantallaInicial( modifier : Modifier = Modifier){
        verticalArrangement = Arrangement.Center,
        horizontalAlignment = Alignment.CenterHorizontally
    ){
+       var mensaje by remember {
+           mutableStateOf("Información del transporte público de San Luis")
+       }
        Text( text = "TransporteSL",
              style = MaterialTheme.typography.headlineMedium)
        Spacer( modifier = Modifier.height(12.dp))
-       Text( text = "Información del transporte público de San Luis")
+       Text( text = mensaje)
        Spacer( modifier = Modifier.height(24.dp))
-       Button(onClick = {})
+       Button(onClick = {
+           mensaje = "Bienvenido!"
+       })
        {
            Text( text = "Continuar",
                  style = MaterialTheme.typography.titleLarge)
